e2e3cdf3b620aeddbb8b4af1d5725bf26b3bd6ec
Matheus Oliveira Gomes <matheusoliveira2899@outlook.com.br>
2020-07-28 21:22:56 -0300

    alteracoes-header-clinica

--- CeamBrasilClinica/processa_envio.php
+++
@@ -1,9 +0,0 @@
-<?php	
-
-	require "../../../ceambrasil_sendmail/bibliotecas/PHPMailer/Exception.php";
-	require "../../../ceambrasil_sendmail/bibliotecas/PHPMailer/OAuth.php";
-	require "../../../ceambrasil_sendmail/bibliotecas/PHPMailer/PHPMailer.php";
-	require "../../../ceambrasil_sendmail/bibliotecas/PHPMailer/POP3.php";
-	require "../../../ceambrasil_sendmail/bibliotecas/PHPMailer/SMTP.php";
-
-	require "../../../ceambrasil_sendmail/processa_envio.php";
