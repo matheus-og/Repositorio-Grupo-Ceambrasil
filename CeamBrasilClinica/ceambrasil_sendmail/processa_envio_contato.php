5c4e788d9bda486e7877131f91b9ab47434e651f
Matheus Oliveira Gomes <matheusoliveira2899@outlook.com.br>
2020-08-23 18:44:49 -0300

    analytics-clinica-ok

--- CeamBrasilClinica/ceambrasil_sendmail/processa_envio_contato.php
+++ CeamBrasilClinica/ceambrasil_sendmail/processa_envio_contato.php
@@ -65,7 +65,7 @@
 	try {
 	    //Server settings
 	    $mail->CharSet = 'UTF-8';
-	    $mail->SMTPDebug = true;                                 // Enable verbose debug output
+	    $mail->SMTPDebug = false;                                 // Enable verbose debug output
 	    $mail->isSMTP();                                      // Set mailer to use SMTP
 	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
--- CeamBrasilClinica/trabalhe_conosco.php
+++ CeamBrasilClinica/trabalhe_conosco.php
@@ -16,6 +16,8 @@
       <link rel="stylesheet" type="text/css" href="css/estilos.css">
       <link rel="stylesheet" type="text/css" href="css/outros_estilos.css">
       <title>Contato</title>
+
+   
    </head>
    <body id="body">
       <!--HEADER-->
@@ -32,94 +34,101 @@
                <div class="collapse navbar-collapse font-weight-bold" id="navbarNav">
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item active text-uppercase">
-                        <a class="nav-link azul-escuro" href="index.html">Home</a>
+                        <a class="nav-link azul-escuro " href="index.html">Home </a>
                      </li>
                      <li class="nav-item text-uppercase">
                         <a class="nav-link azul-escuro " href="institucional.html">Institucional</a>
                      </li>
                      <li class="nav-item text-uppercase">
-                        <a class="nav-link azul-escuro" href="planos_atendidos.html">Planos Atendidos</a>
-                     </li>                   
+                        <a class="nav-link azul-escuro" href="informativo.html">Informativo</a>
+                     </li>
+                     <li class="nav-item text-uppercase">
+                        <a class="nav-link azul-escuro" href="planos.php">Planos</a>
+                     </li>
                      <li class="nav-item text-uppercase">
                         <a class="nav-link azul-escuro" href="contato.php">Contato</a>
                      </li>
-                      <li class="nav-item text-uppercase">
-                        <a class="nav-link  text-primary" href="trabalhe_conosco.php"><span class="sr-only">(Página atual)</span>Trabalhe Conosco</a>
+                     <li class="nav-item text-uppercase">
+                        <a class="nav-link text-primary" href="trabalhe_conosco.php"><span class="sr-only">(Página atual)</span>Trabalhe Conosco</a>
                      </li>
-                     
                   </ul>
                </div>
             </div>
             <!--fim container--> 
          </nav>
-         
       </header>
       <!--HEADER-->
-      
       <!--TEXTO ANIMADO-->      
       <section class="bg-primary mb-5">
          <div class="container">
             <div class="row py-5">
-               <div id="banner-animado" href="" class="typewrite bg-primary ml-5 text-white" data-period="2000" data-type='[ "Trabalhe Conosco.", "CeamBrasil.", "Escolha o Destino e Deixe as Preocupações Conosco." ]'> </div>
+               <div id="banner-animado" href="" class="col h5 typewrite bg-primary ml-5 text-white altura-banner" data-period="2000" data-type='[ "Trabalhe Conosco.", "CeamBrasil.", "Escolha o Destino e Deixe as Preocupações Conosco." ]'> </div>
             </div>
          </div>
       </section>
-      <!--/TEXTO ANIMADO-->  
+      <!--/TEXTO ANIMADO--> 
       <section class="container ">
          <div class="row">
             <!-- formulario de contato -->
-            <div class="col-12 col-md-6">            
-               <form class="text-center border border-light mb-5 " action="processa_envio_curriculum.php" method="post">
+            <div class="col-12 col-md-6">
+               <form class="text-center border border-light mb-5 " action="processa_envio_curriculum.php" method="post" role="form" enctype="multipart/form-data">
                   <p class="h4 mb-4 text-primary">Envie seu Curriculum</p>
                   <!-- Nome -->
                   <input type="text" id="nome" name="nome" class="form-control mb-4" placeholder="Nome completo">
                   <!-- Email -->
                   <input type="email" id="email" name="email" class="form-control mb-4" placeholder="E-mail">
                   <!-- celular -->
-                  <input type="text" id="celular" name="celular" class="form-control mb-4" placeholder="Celular">
-                  
+                  <input type="text" id="celular" name="celular" class="form-control mb-4" placeholder="Celular com DDD">
                   <!-- celular -->
                   <div class="form-group">
                      <select class="browser-default custom-select mb-4 form-control" id="area_atuacao" name="area_atuacao">
-                       <option value="" disabled selected>Selecione sua área de atuação</option>
-                       <option value="Administrativo">Administrativo</option>
-                       <option value="2">Recepção</option>
-                       <option value="3">Enfermagem</option>
-                       <option value="4">Serviços Gerais</option>
-                       <option value="5">Médicos</option>
-                       <option value="6">Radiologia</option>
-                       <option value="7">Outros</option>
+                        <option value="" disabled selected>Selecione sua área de atuação</option>
+                        <option value="Administrativo">Administrativo</option>
+                        <option value="Recepcao">Recepção</option>
+                        <option value="Enfermagem">Enfermagem</option>
+                        <option value="Servicos_Gerais">Serviços Gerais</option>
+                        <option value="Medicos">Médicos</option>
+                        <option value="Radiologia">Radiologia</option>
+                        <option value="Outros">Outros</option>
                      </select>
                   </div>
- 
                   <!-- Mensagem -->
                   <div class="form-group">
-                     <textarea class="form-control rounded-0" id="apresentacao" name="apresentacao" rows="3" placeholder="Mensagem"></textarea>
+                     <textarea class="form-control rounded-0" id="apresentacao" name="apresentacao" rows="3" placeholder="Apresente-se e envie seu curriculum em anexo."></textarea>
                   </div>
 
-                  <!--input type="file" id="curriculum" name="curriculum" accept=".pdf"-->
+                  <input class="my-4 form-control" type="file" id="arquivo" name="arquivo" />
 
-               
+           
                   <!-- Botão Enviar -->
-                  <button class="btn btn-primary btn-block" type="submit">Enviar</button>
-               </form><!-- Default form contact -->
+                  <button id="botao-enviar" class="btn btn-primary btn-block" type="submit" data-toggle="collapse" data-target="#circulo" onclick="getElementById('botao-enviar').style.display = 'block'; this.style.display = 'none'">Enviar</button>
+                  
+                  <!--CARREGANDO-->
+                  <div class="collapse" id="circulo" >
+                     <h4 class="py-1 text-primary azul-escuro font-weight-bold">Enviando...</h4>
+                     <div class="carregando ml-2">
+                     </div>
+                  </div>
+                  <!--/CARREGANDO-->
+                  
+
+               </form>
+               <!-- Default form contact -->
             </div>
             <!--detalhes para contato-->
             <div class="col-12 col-md-6 mb-5">
                <p class="text-uppercase font-weight-bold mt-5 ml-5 text-primary">Telefone</p>
                <p class="text-uppercase text-secondary font-weight-bold mt-1 ml-5">35 3629 8000</p>
-
                <p class="text-uppercase font-weight-bold mt-4 ml-5 text-primary">e-mail</p>
                <p class="text-secondary font-weight-bold mt-1 ml-5">contato@ceambrasil.com</p>
-            </div> 
+            </div>
          </div>
       </section>
-      <!--rodape-->
       <footer class="bg-dark text-white">
          <div class="container">
             <div class="row borda-bottom-branca py-3 ">
                <div class="col-12 col-md-6">
-                  <h1 class="text-center">Clínica Ceam</h1>
+                  <h1 class="text-center">Ceam Brasil</h1>
                </div>
                <div class="col-12 col-md-2 ">          
                </div>
@@ -133,22 +142,22 @@
                   <span class="h5 font-weight-bold text-white text-uppercase">Endereço</span>
                   <p>
                      <a href="https://goo.gl/maps/roEmKh9SKAQtjJEK6" class="text-white  pt-2 mb-3" target="_blank">
-                        <i class="fas fa-map-marker-alt mr-2 "></i>
-                        Praça Doutor Carlos Victor, n° 01 <br>
-                        Bairro: Varginha Itajubá - MG <br>
-                        CEP: 37501-155
+                     <i class="fas fa-map-marker-alt mr-2 "></i>
+                     Praça Doutor Carlos Victor, n° 01 <br>
+                     Bairro: Varginha Itajubá - MG <br>
+                     CEP: 37501-155
                      </a>
                   </p>
-                 <p class="text-white mt-3">
-                            <i class="fas fa-phone-alt mr-2 mb-3"></i>
-                            <a href="tel:35362980000" class="text-white">(35) 3629-8000</a>
-                        </p>
+                  <p class="text-white mt-3">
+                     <i class="fas fa-phone-alt mr-2 mb-3"></i>
+                     <a href="tel:35362980000" class="text-white">(35) 3629-8000</a>
+                  </p>
                   <span class="h5 font-weight-bold text-white text-uppercase">Sites</span><br>
-                  <a href="http://www.ceambrasil.com.br" target="_blank" class="text-white mt-3">
+                  <a href="http://www.clinicaceam.com.br/" target="_blank" class="text-white mt-3">
                   <i class="fas fa-globe mr-2 mb-3"></i>
-                  Ceam Brasil
+                  Clinica Ceam
                   </a><br>  
-                  <a href="http://grupoceam.com.br" target="_blank" class="text-white mt-3">
+                  <a href="http://grupoceam.com.br/" target="_blank" class="text-white mt-3">
                   <i class="fas fa-globe mr-2"></i>
                   Grupo Ceam
                   </a>   
@@ -164,8 +173,11 @@
                            <a class="nav-link text-white" href="institucional.html">Institucional</a>
                         </li>
                         <li class="nav-item ">
-                           <a class="nav-link text-white" href="planos_atendidos.html">Planos Atendidos</a>
-                        </li>                                        
+                           <a class="nav-link text-white" href="informativo.html">Informativo</a>
+                        </li>
+                        <li class="nav-item ">
+                           <a class="nav-link text-white" href="planos.php">Planos</a>
+                        </li>
                      </ul>
                   </div>
                </div>
@@ -188,13 +200,16 @@
             <div class="row">
                <div class="col-12 col-md-4 mb-2 d-flex justify-content-center flex-wrap">
                   <div class="flex-item">
-                     <a href="http://www.ans.gov.br" target="_blank"><img src="images/ceamclinica.png" class="img w-75 pt-2 "></a>
-                  </div>           
+                     <a href="http://www.ans.gov.br" target="_blank"><img src="images/ans.png" class="img w-75 pt-2 "></a>
+                  </div>
+                  <div class=" py-1 flex-item">
+                     <span class="font-weight-bold text-white border">ANS - N° 311472</span>
+                  </div>
                </div>
                <div class="col-6 col-md-4  d-flex justify-content-center ">
                   <div class="flex-item mt-4 ">
                      <br>
-                     <p class="text-white">CNPJ 10.786.528/0001-91</p>
+                     <p class="text-white">CNPJ 18.987.107/0001-30</p>
                   </div>
                </div>
                <div class="col-6 col-md-4 d-flex justify-content-center">
@@ -205,16 +220,38 @@
             </div>
          </div>
       </footer>
-      <!--/rodape-->
+      <!-- The scroll to top feature -->
+      <a class="scroll-top-wrapper bg-primary text-white" href="#body">
+      <span class="scroll-top-inner">
+      <i class="fa fa-2x fa-arrow-circle-up"></i>
+      </span>
+      </a>
       <!-- JavaScript (Opcional) -->
       <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+         <script type="text/javascript">  
+          $(document).ready(function(){
+              $('#celular').mask('(00) 00000-0000');
+            });
+      </script>
+
       <!--SOBRE EMPRESA-->
       <script src="owl-carousel/owl.carousel.min.js"></script>
       <script src="js/main.js"></script>
       <script src="js/main2.js"></script>
       <script src="js/top.js"></script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163974216-2">
+      </script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163974216-2');
+      </script>
    </body>
 </html>
